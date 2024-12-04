d7b8f4bc8fc44dfdb919591ba9f2d0600c16c612
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2024-12-04 03:51:45 +0000

    Polish template

--- template/src/do.zig
+++ template/src/do.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 /// Perform main business logic.
-pub fn stuff(input: []u8) !void {
+pub fn stuff(input: []u8, alloc: std.mem.Allocator) !void {
+    _ = alloc;
     std.debug.print("Hello world!\n{s}", .{input});
 }
--- template/src/main.zig
+++ template/src/main.zig
@@ -2,10 +2,11 @@ const std = @import("std");
 const do = @import("do.zig");
 
 const MAX_SIZE = 1_000_000_000;
+const FILE_NAME = "test-input";
 
 pub fn main() !void {
     // setup file
-    const file = std.fs.cwd().openFile("input", .{ .mode = .read_only }) catch |e| {
+    const file = std.fs.cwd().openFile(FILE_NAME, .{ .mode = .read_only }) catch |e| {
         std.debug.print("Failed to open file with: {any}\n", .{e});
         return e;
     };
@@ -20,5 +21,5 @@ pub fn main() !void {
     defer alloc.free(input);
 
     // perform business logic
-    try do.stuff(input);
+    try do.stuff(input, alloc);
 }
